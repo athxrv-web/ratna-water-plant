f4cb2aa1b8357c8f4c8225058e1ee23289422ec9
athxrv-web <atharv7587@gmail.com>
2026-03-07 14:16:37 +0530

    Rename app from 'core' to 'waterjar.core'

--- waterjar/core/apps.py
+++ waterjar/core/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+    name = "waterjar.core"
     verbose_name = "Ratna Water Plant"
